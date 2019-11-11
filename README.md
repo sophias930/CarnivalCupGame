3ddb8a25ae107463d72a7a38a7c4122a72c4b7cb
Sophia Shaikh <sophias930@gmail.com>
2019-11-10 21:05:18 -0500

    initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+HELPFUL HINTS:
+
+- The cups that fall can be all the cups on the row you click on and above!
---
+++ src/main/CupView.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class CupView extends JPanel {
+	
+	Cup cup;
+	
+	public CupView(Cup c) {
+		this.cup = c;
+	}
+
+}
